6ae33ffca9f360dc440b68e79b491511c5df223e
Antonio Cascais <acascais@indocresearch.org>
2023-12-13 11:34:03 +0100

    Added neural activity cubic code
    Also deleted demo jupyter notebook in favour of the final version

---
+++ neuralactivitycubic/core.py
@@ -0,0 +1,91 @@
+import imageio.v3 as iio
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+from matplotlib.ticker import MultipleLocator
+
+from typing import Tuple
+
+
+
+class Square:
+
+    def __init__(self, upper_left_corner_coords: Tuple[int, int], cropped_image_stack: np.ndarray) -> None:
+        self.upper_left_corner_coords = upper_left_corner_coords
+        self.image_stack = cropped_image_stack
+        self.center_coords = self._get_center_coords()
+        
+
+    def compute_mean_intensity_timeseries(self) -> None:
+        self.intensities = np.mean(self.image_stack, axis = (1,2))
+
+
+    def detect_peaks(self, signal_to_noise_ratio: float) -> None:
+        self.frame_idxs_of_peaks = signal.find_peaks_cwt(vector = self.intensities, wavelet = signal.ricker, widths = np.arange(1, 81), min_length = 7, noise_perc = 10, min_snr = signal_to_noise_ratio)
+        self.peaks_count = self.frame_idxs_of_peaks.shape[0]
+
+
+    def plot_intensity_trace_with_identified_peaks(self) -> None:
+        self.plotted_trace = plt.figure(figsize = (12, 3), facecolor = 'white')
+        self.plotted_trace = plt.plot(intensities, zorder = 0, c='black')
+        self.plotted_trace = plt.scatter(x = frame_idxs_of_peaks, y = intensities[frame_idxs_of_peaks], color = 'red')
+        self.plotted_trace = plt.ylabel('mean bit value')
+        self.plotted_trace = plt.xlabel('frame index')
+        self.plotted_trace = plt.title(f'Graph "x-idx", "y-idx"    Total Activity: {frame_idxs_of_peaks.shape[0]}')
+        self.plotted_trace = plt.tight_layout()
+
+
+    def _get_center_coords(self) -> Tuple[int, int]:
+        square_height = self.image_stack.shape[1]
+        square_width = self.image_stack.shape[2]
+        return (self.upper_left_corner_coords[0] + int(square_height/2), self.upper_left_corner_coords[1] + int(square_width/2))
+
+
+def run_analysis(video_filepath, window_size, signal_to_noise_ratio):
+    frames = iio.imread(video_filepath)
+    gray_scale_frames = frames[:, :, :, 0].copy()
+    
+    
+    grid_rows = np.arange(start = 0, stop = gray_scale_frames.shape[1], step = window_size)
+    grid_cols = np.arange(start = 0, stop = gray_scale_frames.shape[2], step = window_size)
+    
+    square_upper_left_coords = []
+    
+    for row in grid_rows:
+        for col in grid_cols:
+            square_upper_left_coords.append((row, col))
+    
+    
+    all_squares = []
+    for upper_left_y, upper_left_x in square_upper_left_coords:
+        square_y_coords_slice = slice(upper_left_y, upper_left_y + window_size)
+        square_x_coords_slice = slice(upper_left_x, upper_left_x + window_size)
+        selected_square = gray_scale_frames[:, square_y_coords_slice, square_x_coords_slice]
+        all_squares.append(Square(upper_left_corner_coords = (upper_left_y, upper_left_x), cropped_image_stack = selected_square))
+    
+    for square in all_squares:
+        square.compute_mean_intensity_timeseries()
+        square.detect_peaks(signal_to_noise_ratio = signal_to_noise_ratio)
+
+
+    sizes = [square.peaks_count for square in all_squares]
+    # creating the plot
+    fig, ax = plt.subplots()
+    # drawing the circles
+    for square in all_squares:
+        true_size = (square.peaks_count/max(sizes))*(window_size/2)
+        circle = plt.Circle((square.center_coords[1], square.center_coords[0]), radius=true_size, fill=False, color='red')
+        ax.add_patch(circle)
+    
+    # putting the image in the background
+    background = gray_scale_frames[0]
+    ax.imshow(gray_scale_frames[0], cmap="gray")
+    
+    # putting the grid on top of the image
+    ax.grid(color = 'gray', linestyle = '--', linewidth = 1)
+    
+    # setting the grid size to the square size
+    ax.xaxis.set_major_locator(MultipleLocator(window_size))
+    ax.yaxis.set_major_locator(MultipleLocator(window_size))
+    plt.savefig('overview2.png')
+    plt.close()
---
+++ neuralactivitycubic/interface.py
@@ -0,0 +1,104 @@
+import ipywidgets as w
+from IPython.display import display
+from .core import run_analysis
+
+# Button to load a movie file from the user local computer
+video_filepath = w.Text(value = 'spiking_neuron.avi')
+
+# Numeric text field for window size
+window_size_text = w.BoundedIntText(
+    value=12,
+    min=0,
+    max=100,
+    step=1,
+    description='Window Size:',
+)
+
+# Button to process image stack
+process_button = w.Button(
+    description='Process Image Stack',
+    button_style='', # 'success', 'info', 'warning', 'danger' or ''
+    tooltip='Process Image Stack',
+)
+
+# Sliders for Signal to Noise Ratio, Signal Average Threshold, and Include Variance
+snr_slider = w.IntSlider(
+    value=3,
+    min=0,
+    max=10,
+    step=1,
+    description='Signal to Noise Ratio:',
+)
+
+sat_slider = w.IntSlider(
+    value=15,
+    min=0,
+    max=30,
+    step=1,
+    description='Signal Average Threshold:',
+    disabled = True
+)
+
+variance_slider = w.IntSlider(
+    value=30,
+    min=0,
+    max=100,
+    step=1,
+    description='Include Variance:',
+    disabled = True
+)
+
+# Checkbox for General Activity Tendency
+activity_checkbox = w.Checkbox(
+    value=False,
+    description='General Activity Tendency',
+    disabled = True
+)
+
+# IntText for Minimum Activity Counts
+min_activity_text = w.BoundedIntText(
+    value=1,
+    min=0,
+    max=100,
+    step=1,
+    description='Minimum Activity Counts:',
+    disabled = True
+)
+
+# Buttons for Detect Activity and Reset
+detect_button = w.Button(
+    description='Detect Activity',
+    button_style='info', # 'success', 'info', 'warning', 'danger' or ''
+    tooltip='Detect Activity',
+    disabled = True
+)
+
+reset_button = w.Button(
+    description='Reset',
+    button_style='danger', # 'success', 'info', 'warning', 'danger' or ''
+    tooltip='Reset',
+    disabled = True
+)
+
+# Layout the w
+widget = w.VBox([video_filepath,
+                window_size_text,
+                process_button,
+                snr_slider,
+                sat_slider,
+                activity_checkbox,
+                variance_slider,
+                min_activity_text,
+                w.HBox([detect_button, reset_button])])
+
+def show():
+    display(widget)
+
+def on_process_button_clicked(b):
+    run_analysis(video_filepath = video_filepath.value, 
+                 window_size = window_size_text.value,
+                 signal_to_noise_ratio = snr_slider.value)
+    
+
+# Attach the callback function to the load button
+process_button.on_click(on_process_button_clicked)
