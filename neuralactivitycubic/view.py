20cd6a8189dd5882a4d2c3d14490ec0156b61d37
Matvey Loshakov <mloshakov@indocresearch.org>
2025-07-16 10:11:50 +0200

    Bug fix

--- neuralactivitycubic/view.py
+++ neuralactivitycubic/view.py
@@ -801,13 +801,13 @@ class WidgetsInterface:
     def _enable_window_size_widgets(self, change) -> None:
         if change['name'] == 'value':
             if change['new'] == 'file':
-                change_widget_state(self.analysis_settings_panel.user_settings_grid_size, disabled = True)
+                change_widget_state(self.analysis_settings_panel.settings.grid_size, disabled = True)
                 change_widget_state(self.analysis_settings_panel.preview_window_size_button, disabled = True)
             else:
-                if self.analysis_settings_panel.user_settings_signal_to_noise_ratio.disabled:
+                if self.analysis_settings_panel.ettings.signal_to_noise_ratio.disabled:
                     pass
                 else:
-                    change_widget_state(self.analysis_settings_panel.user_settings_grid_size, disabled = False)
+                    change_widget_state(self.analysis_settings_panel.settings.grid_size, disabled = False)
                     change_widget_state(self.analysis_settings_panel.preview_window_size_button, disabled = False)                
 
 
